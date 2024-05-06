93c23547256da273ffc0b5fa1a5e1088edef6b98
ndrrmdhn <andreramadhani98@gmail.com>
2024-05-06 22:08:47 +0700

    Style Navbar & Make Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+
+import NavbarComponent from "./components/NavbarComponent";
+// import FooterComponent from "./components/FooterComponent";
+
+import Homepage from "./pages/HomePage";
+import KelasPage from "./pages/KelasPage";
+import TestimonialPage from "./pages/TestimonialPage";
+import FaqPage from "./pages/FaqPage";
+import SyaratKetenPage from "./pages/SyaratKetenPage";
 
 function App() {
-  const [count, setCount] = useState(0)
+  return <div>
+    <NavbarComponent />
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" Component={Homepage}/>
+      <Route path="/kelas" Component={KelasPage}/>
+      <Route path="/testimonial" Component={TestimonialPage}/>
+      <Route path="/faq" Component={FaqPage}/>
+      <Route path="/syaratketen" Component={SyaratKetenPage}/>
+    </Routes>
+
+    {/* <FooterComponent /> */}
+  </div>;
 }
 
 export default App
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <header className="w-100 min-vh-100"></header>
+    </div>
+  )
+}
+
+export default Homepage
